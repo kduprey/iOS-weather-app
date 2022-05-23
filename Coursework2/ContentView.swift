c084e2b554f1ce4f4081e565e3ab013138d564ce
Kenton Duprey <kentonduprey@icloud.com>
2022-05-23 16:24:52 -0400

    Initial Commit

---
+++ Coursework2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coursework2
+//
+//  Created by Kenton Duprey on 5/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Coursework2/Coursework2App.swift
@@ -0,0 +1,17 @@
+//
+//  Coursework2App.swift
+//  Coursework2
+//
+//  Created by Kenton Duprey on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Coursework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
